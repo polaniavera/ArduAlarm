74143226bb61c5b8c406022e82713796e44fe61b
Cristhian Polania <polaniavera@gmail.com>
2019-05-15 03:05:34 +1000

    Update arduAlarm.ino

--- arduAlarm/arduAlarm.ino
+++ arduAlarm/arduAlarm.ino
@@ -8,12 +8,24 @@ mensaje
 */
 
 SoftwareSerial gsmSerial(7, 8); // Tx, Rx
-String readString="";
+String readString = "";
 String number = "";
 String message = "";
 String users = "";
-String onAlarm = "ON";
-const int buzzer = 10;
+String answer = "";
+const String configuration = "C";
+const String fire = "I";
+const String evacuation = "E";
+const String minute = "M";
+const String second = "S";
+const String specialType = "161";
+const String standardType = "129";
+const String msgFire = "incendio";
+const String msgEvacuation = "evacuacion";
+const String msgGeneral = "hurto";
+const int outFire = 10;
+const int outEvacuation = 11;
+const int outGeneral = 12;
 String password = "1234";
 
 void setup()
@@ -21,14 +33,22 @@ void setup()
   gsmSerial.begin(9600); // Setting the baud rate of GSM Module
   Serial.begin(9600); // Setting the baud rate of Serial Monitor
   delay(200);
-  pinMode(buzzer,OUTPUT);
+  pinMode(outFire,OUTPUT);
+  pinMode(outEvacuation,OUTPUT);
+  pinMode(outGeneral,OUTPUT);
   receiveMessage();
   getUsers();
+/**********************************************************************
+  Leer EEPROM
+***********************************************************************/
   Serial.println("Device Setup Complete.");
 }
 
 void loop() {
-  checkMessages(); // Check for messages
+  checkMessages();
+/**********************************************************************
+  Chequear llamada
+***********************************************************************/
 }
 
 void checkMessages() {
@@ -42,8 +62,7 @@ void checkMessages() {
       message = readString.substring(startMessage+1,readString.length()-1);
       message = message.substring(message.indexOf('\n')+1,message.length()-1);
       validate();
-    }
-    else{
+    }else{
       Serial.println("El modulo esta activo y escuchando mensajes SMS.");
     }
   }
@@ -51,87 +70,86 @@ void checkMessages() {
 
 void validate(){
   message.toUpperCase();
-  if(message.substring(0, 1).equals("C")){
+  if(message.substring(0, 1).equals(configuration)){
     setConfig();
   }else if(checkUser()){
-    if(message.equals(onAlarm)>0){
-    arm();
-    }
-    else{
-      Serial.println("Comando Incorrecto");
+    if(message.equals(fire)){
+      setAlarm(outFire, msgFire);
+    }else if(message.equals(evacuation)){
+      setAlarm(outEvacuation, msgEvacuation);
+    }else{
+      setAlarm(outGeneral, msgGeneral);
     }
   }else{
-    Serial.println("Usuario no encontrado");
+    answer = "Usuario no encontrado";
+    sendMessage(number);
+    Serial.println(answer);
   }
 }
 
 void setConfig(){
-  //C1: Configuracion de user
-  //C2: Configuracion de tiempos de salida relevada y salida voz
+  //C1: Usuario
+  //C2: Tiempos de sirena y perifoneo
   //C3: Cambiar password
-  //C4: Borrar
+  //C4: Borrar usuario
   //C5: test
-
   switch (message.substring(1, 2).toInt()) {
   case 1:
-    //SET USER/TYPE 
-    //FORMAT MSN [C1,CLAVE DE 4 DIGITOS, NUMERO DE 10 DIGITOS, NOMBRE SOLO HASTA 14 DIGITOS]
-    String rtnUser = setUser();
-    Serial.println(rtnUser);
+    //[C1,CLAVE,TIPO[1/0](1),NUMERO(10),NOMBRE(MAX 14)]
+    answer = setUser();
     getUsers();
     break;
   case 2:
-    //SET TIME OUTPUT: RELE & VOZ 
-    //FORMAT MSN [C2,CLAVE 4 DIGITOS, MINUOTOS/SEGUNDOS DE 1 DIGITO, TIEMPO SALIDA RELE (DOS DIGITOS), TIMEPO SALIDA VOZ(2 DIGITOS)]
-    String rtnTout = setTimeOut();
-    Serial.println(rtnTout);
+    //[C2,CLAVE,UNIDAD[M/S](1),TIEMPO RELE(2),TIEMPO VOZ(2)]
+    answer = setTimeOut();
     break;
   case 3:
-    //SET CHANGE/UPGRADE PASSWORD
-    //FORMT MSN [C3,CLAVE (4 DIGITOS),CLAVE NUEVA (4DIGITOS), CLAVE NUEVA CONFIRMACION (4DIGITOS)]
-    String rtnPass = setPassword();
-    Serial.println(rtnPass);
+    //[C3,CLAVE,CLAVE NUEVA(4),CLAVE NUEVA CONFIRMACION(4)]
+    answer = setPassword();
     break;
   case 4:
-    //SET DELETE
-    //FORMAT MSN[C4,CLAVE(4DIGITOS), SELECCION DE BORRADO (1 o 2 DIGITO(s))]
-    // SELECCION DE BORRADO, SI EL DIGITO ES IGUAL 'A' = BORRADO TOTAL
-    //                       SI EL DIGITO ES IGUAL 'NUMERO ENTERO DEL 1-53'
-    String rtnDel = deleteUsers();
-    Serial.println(rtnDel);
+    //[C4,CLAVE(4),BORRADO[A/1-53](1/2))]
+    answer = deleteUsers();
     getUsers();
     break;
   case 5:
-    //SET TEST SYSTEM
-    //FORMAT MSN [C5,CLAVE(4DIGITOS)]
-    //testea haciendo una llamada perdida a los numeros standar
-    //y envia mensaje a contactos numeros especiales
-    
-    testSystem();
+    //[C5,CLAVE]
+    answer = testSystem();
     break;
   default:
-    Serial.println("Formato incorrecto");
+    answer = "Formato incorrecto";
     break;
   }
+  sendMessage(number);
+  Serial.println(answer);
 }
 
 //********************************CONFIGURACION*************************************
 //**********************************************************************************
 String setUser(){
-  if(!(message.charAt(2)==',' && message.charAt(7)==',' && message.charAt(18)==',')){
+  //Tipo usuario: 1 especial (161), 0 standard (129)
+  if(!(message.charAt(2)==',' && message.charAt(7)==',' && message.charAt(9)==',' && message.charAt(20)==',')){
     return "formato incorrecto C1";
   }
   if(!checkPass()){
     return "Clave incorrecta";
   }
-  String numero = message.substring(8,18);
-  if(!CheckNumber(numero)){
+  String type = "";
+  if(message.substring(8,9).equals("1")){
+    type = specialType;
+  }else if(message.substring(8,9).equals("0")){
+    type = standardType;
+  }else{
+    return "Tipo de usuario incorrecto";
+  }
+  String numberUser = message.substring(10,20);
+  if(!CheckNumber(numberUser)){
     return "Numero telefonico incorrecto";
   }
-  String nombre = message.substring(19,message.length());
+  String nameUser = message.substring(21, message.length());
   gsmSerial.println("AT+CPBS= \"SM\"");
   delay(300);
-  gsmSerial.println("AT+CPBW=,\""+numero+"\",,\""+nombre+"\"");
+  gsmSerial.println("AT+CPBW=,\""+numberUser+"\","+type+",\""+nameUser+"\"");
   delay(300);
   return "Contacto almacenado";
 }
@@ -143,24 +161,25 @@ String setTimeOut(){
   if(!checkPass()){
     return "Clave incorrecta";
   }
-  String unidad = message.substring(8,9);
-  if(!(unidad.equals("M") || unidad.equals("S"))){
+  String unity = message.substring(8,9);
+  if(!(unity.equals(minute) || unity.equals(second))){
     return "Unidad incorrecta";
   }
   String tRele = message.substring(10,12);
   if(!CheckNumber(tRele)){
     return "Tiempo de relevo incorrecto";
   }
-  String tVoz = message.substring(13,15);
-  if(!CheckNumber(tVoz)){
+  String tVoice = message.substring(13,15);
+  if(!CheckNumber(tVoice)){
     return "Tiempo de voz incorrecto";
   }
-
-  return "UNIDAD:"+unidad+"\n"+"tiempo salida tele:"+tRele+"\n"+"tiempo salida voz:"+tVoz;
+/**********************************************************************
+  Pasar valores de tOut a EEPROM
+***********************************************************************/
+  return "Unidad:"+unity+", tiempo tele:"+tRele+", tiempo voz:"+tVoice;
 }
 
 String setPassword(){
-  //FMT MSN [C3,1234,5678,5678]
   if(!(message.charAt(2)==',' && message.charAt(7)==',' && message.charAt(12)==',')){
     return "Formato incorrecto C3";
   }
@@ -170,13 +189,19 @@ String setPassword(){
   String newPass = message.substring(8,12);
   String confirmPass = message.substring(13,17);
   if(newPass.equals(confirmPass)){
-    return "Clave antigua:"+message.substring(3,7)+"\n"+"clave nueva:"+newPass+"\n"+"confirmacion clave:"+confirmPass;  
+/**********************************************************************
+  Pasar valores de clave a EEPROM
+***********************************************************************/
+    return "Clave nueva:"+newPass+", clave antigua:"+message.substring(3,7);  
   }else{
     return "Claves no coinciden";
   }
 }
 
 String deleteUsers(){
+/**********************************************************************
+  Pendiente
+***********************************************************************/
   for (int i=1; i<20; i++){
     //String num = "AT+CPBW="+(String)i+"\n\r";
     gsmSerial.println("AT + CPBW ="+(String)i+"\r");
@@ -186,40 +211,67 @@ String deleteUsers(){
   String retorno2= gsmSerial.readString();
   //int largo= retorno2.length();
   retorno2 = retorno2.substring(retorno2.indexOf('\n' )+5,retorno2.length()-41);
-  Serial.print("estado de procedimiento: "+retorno2);
+  return "estado de procedimiento: "+retorno2;
 }
 
-void testSystem(){
-  
+String testSystem(){
+/**********************************************************************
+  Pendiente
+***********************************************************************/
+  return "Test";
 }
 
 //**********************************OPERACION***************************************
 //**********************************************************************************
-void arm () {
-  Serial.println("ALERTA!");
-  sendMessage();
-  digitalWrite(buzzer,HIGH);
+void setAlarm(int out, String msg){
+  digitalWrite(out,HIGH);
+  answer = "El usuario con numero telefonico "+number+" activo alarma de "+msg;
+  Serial.println(answer);
+  smsSpecialUsers();
   delay(10000);
-  digitalWrite(buzzer,LOW);
+  digitalWrite(out,LOW);
 }
 
-void sendMessage() {
-  String mobileNumber = "";
+void smsSpecialUsers(){
+  char delimiter[] = "+";
+  String temp = "";
+  String numSpecial = "";
+  
+  //Convertir String users a char[]
+  char charCon[users.length() + 1];
+  users.toCharArray(charCon, users.length() + 1);
+  
+  //Pasar char[] a String, obtener tipo y enviar sms
+  char* ptr = strtok(charCon, delimiter);
+  while(ptr != NULL) {
+    temp = String(ptr);
+    int startNumber = temp.indexOf('"');
+    int endNumber = temp.indexOf('"', startNumber+1);
+    if(temp.substring(endNumber+2, endNumber+5).equals(specialType)){
+      numSpecial = temp.substring(startNumber+1, endNumber);
+      sendMessage(numSpecial);
+    }
+    ptr = strtok(NULL, delimiter);
+  } 
+}
+
+void sendMessage(String n) {
   gsmSerial.println("AT+CMGF=1"); //Sets the GSM Module in Text Mode
   delay(1000);
-  gsmSerial.println("AT+CMGS=\""+mobileNumber+"\"\r");
+  gsmSerial.println("AT+CMGS=\""+n+"\"\r");
   delay(1000);
-  gsmSerial.println("Alerta de seguridad!");
+  gsmSerial.println(answer);
   delay(100);
   gsmSerial.println((char)26);
   delay(1000);
+/**********************************************************************
+  Revisar respuesta
+***********************************************************************/  
+  Serial.println("mensaje enviado a "+n);
 }
 
 bool checkUser(){
-  if(users.indexOf(number) > 0){
-    return true;
-  }
-  return false;
+  return users.indexOf(number) > 0 ? true : false;
 }
 
 //*******************************UTILIDADES*****************************************
